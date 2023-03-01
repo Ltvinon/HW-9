dbf1ddc910b48e8f7225584c6b6724ef98905d83
Andrii <ltvinon@gmil.com>
2023-03-01 17:54:21 +0200

    Add project files.

---
+++ HW-9/Program.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace HW_9
+{
+    using System.Diagnostics;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            RecursiveMethod();
+        }
+
+        private static void RecursiveMethod()
+        {
+            var thread = new Thread(RecursiveMethod);
+            thread.IsBackground = true;
+            thread.Start();
+            Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}, Total Threads: {Process.GetCurrentProcess().Threads.Count}");
+            thread.Join();
+        }
+    }
+}
